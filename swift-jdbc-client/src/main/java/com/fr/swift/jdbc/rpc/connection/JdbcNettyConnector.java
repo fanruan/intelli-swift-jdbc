1f0772f79be3d6bf4e21cfa1e42dd547817704c4
Yee <yee@fanruan.com>
2019-08-30 15:16:05 +0800

    DEC-9508 swift测试数据库sql报错 BugFix Rpc不稳定，换个Netty的实现

---
+++ swift-jdbc-client/src/main/java/com/fr/swift/jdbc/rpc/connection/JdbcNettyConnector.java
@@ -0,0 +1,73 @@
+package com.fr.swift.jdbc.rpc.connection;
+
+import com.fr.swift.jdbc.rpc.JdbcExecutor;
+import com.fr.swift.jdbc.rpc.invoke.BaseConnector;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.util.Assert;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+/**
+ * @author yee
+ * @version 1.1
+ * Created by yee on 2019-08-30
+ */
+public class JdbcNettyConnector extends BaseConnector {
+    private EventLoopGroup group;
+
+    public JdbcNettyConnector(String address) {
+        super(address);
+    }
+
+    public JdbcNettyConnector(String host, int port) {
+        super(host, port);
+    }
+
+    @Override
+    public void notifySend() {
+        rpcExecutors.get(0).send(getRequest());
+    }
+
+    @Override
+    public void start() {
+        group = new NioEventLoopGroup(1);
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group);
+        bootstrap.channel(NioSocketChannel.class);
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel channel) {
+                ChannelPipeline pipeline = channel.pipeline();
+                pipeline.addLast(
+                        new ObjectDecoder(1000000000, ClassResolvers.cacheDisabled(this
+                                .getClass().getClassLoader())));
+                pipeline.addLast(new ObjectEncoder());
+                final JdbcExecutor jdbcExecutor = rpcExecutors.get(0);
+                Assert.isTrue(jdbcExecutor instanceof ChannelHandler);
+
+                pipeline.addLast((ChannelHandler) jdbcExecutor);
+            }
+        });
+        bootstrap.option(ChannelOption.TCP_NODELAY, true);
+        try {
+            bootstrap.connect(host, port).sync();
+        } catch (InterruptedException e) {
+            SwiftLoggers.getLogger().error(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        group.shutdownGracefully();
+    }
+}
