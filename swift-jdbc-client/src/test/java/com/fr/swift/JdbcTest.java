f61f4cb60d2120c640577c45b9704e7e25df8cff
lucifer <lucifer@fanruan.com>
2020-11-19 19:21:06 +0800

    KERNEL-6215 本地测试，输出测试说明，调整代码和结构 1、本地联调sql、接口等，跑通fr数据 2、数据块增加rows等信息 3、预留快照、回滚等接口

--- swift-jdbc-client/src/test/java/com/fr/swift/JdbcTest.java
+++ swift-jdbc-client/src/test/java/com/fr/swift/JdbcTest.java
@@ -7,6 +7,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yee
@@ -14,17 +16,53 @@ import java.sql.SQLException;
  */
 public class JdbcTest {
     @Test
-    public void test() throws ClassNotFoundException, SQLException {
-        String sql1 = "select app_id,year_month,count(app_id) from TemplateDetail";
-
-        String sql2 = "select * from TemplateDetail";
+    public void test() throws ClassNotFoundException, SQLException, InterruptedException {
+//        String sql1 = "select app_id,year_month,count(app_id) from TemplateDetail";
+//
+//        String sql2 = "select * from TemplateDetail";
 
         Class.forName("com.fr.swift.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:swift:remote://127.0.0.1:7000/cube", "cloudswift", "iloveswift");
-        query(connection, sql1);
-        System.out.println();
-        query(connection, sql2);
+        List<String> list = new ArrayList<>();
+//        list.add("select count(swift_app_id) from ConsumeProportion");
+//        list.add("select count(swift_app_id) from ConsumeTime");
+//        list.add("select count(swift_app_id) from Days");
+//        list.add("select count(swift_app_id) from DownTimeAnalysis");
+//        list.add("select count(swift_app_id) from DownTimes");
+//        list.add("select count(swift_app_id) from IsFr");
+//        list.add("select count(swift_app_id) from JarVersion");
+//        list.add("select count(swift_app_id) from Last2MonthDowntime");
+//        list.add("select count(swift_app_id) from Last2MonthTimesAndPersonTimes");
+//        list.add("select count(swift_app_id) from Last3MonthDowntime");
+//        list.add("select count(swift_app_id) from Last3MonthUpload");
+//        list.add("select count(swift_app_id) from LastMonthDowntime");
+//        list.add("select count(swift_app_id) from LastMonthTimesAndPersonTimes");
+//        list.add("select count(swift_app_id) from LastMonthUpload");
+//        list.add("select count(swift_app_id) from MemoryBalanceScore");
+//        list.add("select count(swift_app_id) from MemoryBalanceScoreHastep");
+//        list.add("select count(swift_app_id) from MemoryConclusion");
+//        list.add("select count(swift_app_id) from MemoryDayTimes");
+//        list.add("select count(swift_app_id) from MemRatioCPU");
+//        list.add("select count(swift_app_id) from ReportConsumeProportion");
+//        list.add("select count(swift_app_id) from RetentionDeclineTopTen");
+//        list.add("select count(swift_app_id) from RetentionRiseTopTen");
+//        list.add("select count(swift_app_id) from SQLConsumeProportion");
+//        list.add("select count(swift_app_id) from TemplateRank");
+//        list.add("select count(swift_app_id) from TimesAndUserNum");
+//        list.add("select count(swift_app_id) from VisitDays");
+//        list.add("select count(swift_app_id) from VisitTimesAndUserTimes");
+        list.add("select count(swift_app_id) from YearMonth");
+        for (String sql : list) {
+            query(connection, "select * from MemRatioCPU where swift_app_id = '836d15a6-8c39-46d0-bbd1-2b5733f74408' order by cpu desc ");
+        }
         connection.close();
+
+
+//        long query1 = System.currentTimeMillis();
+//        System.out.println(query1-start);
+//        query(connection, sql2);
+//        long query2 = System.currentTimeMillis();
+//        System.out.println(query2-query1);
     }
 
     public void query(Connection connection, String sql) throws SQLException {
